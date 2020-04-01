4ffdca7dcbe64a4678621e9848c3f28d92fddfdd
Eugeny Zhiharev <jjuchara@gmail.com>
2020-04-01 16:25:58 +0300

    React-redux second commit

--- src/App.js
+++ src/App.js
@@ -1,80 +1,20 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import classes from './App.module.css';
 import CommentsList from "./components/CommentsList/CommentsList";
 import Comment from "./components/CommentsList/Comment/Comment";
 import TextArea from "./components/TextArea/TextArea";
 import Input from "./components/Input/Input";
 import Button from "./components/Button/Button";
+import {onInputChangeHandler, onTextAreaChangeHandler} from "./redux/actions/actions";
 
 class App extends Component {
 
-    state = {
-        fieldsValid: false,
-        comments: [],
-        comment: {
-            commentText: '',
-            name: '',
-            date: new Date().toLocaleString()
-        },
-        textArea: {
-            errorMessage: 'Поле не должно быть пустым',
-            valid: false,
-            touched: false,
-            validation: {
-                required: true
-            }
-        },
-        input: {
-            errorMessage: 'Поле не должно быть пустым',
-            valid: false,
-            touched: false,
-            validation: {
-                required: true
-            }
-        }
-
-    };
-
-    onTextAreaChangeHandler = e => {
-        const comment = {...this.state.comment};
-        comment.commentText = e.target.value;
-
-        const textArea = {...this.state.textArea};
-        textArea.touched = true;
-        textArea.valid = this.validateControl(this.state.comment.commentText, textArea.validation);
-
-        let fieldsValid = false;
-
-        if (textArea.valid && this.state.input.valid) {
-            fieldsValid = true
-        }
-
-        this.setState({comment, textArea, fieldsValid})
-
-    };
-
-    onInputChangeHandler = e => {
-        const comment = {...this.state.comment};
-        comment.name = e.target.value;
-        const input = {...this.state.input};
-        input.touched = true;
-        input.valid = this.validateControl(this.state.comment.name, input.validation);
-
-        let fieldsValid = false;
-
-        if (input.valid && this.state.textArea.valid) {
-            fieldsValid = true
-        }
-
-
-        this.setState({comment, input, fieldsValid})
-
-    };
 
     onClickBtnHandler = () => {
 
-        const comments = [...this.state.comments];
-        const comment = {...this.state.comment};
+        const comments = [...this.props.comments];
+        const comment = {...this.props.comment};
 
         comments.push(comment);
 
@@ -109,7 +49,7 @@ class App extends Component {
     }
 
     onDelete = (index) => {
-        const comments = [...this.state.comments];
+        const comments = [...this.props.comments];
         comments.splice(index, 1);
 
         this.addToLocalStorage(comments);
@@ -119,7 +59,7 @@ class App extends Component {
     };
 
     renderMessage() {
-        return this.state.comments.map((comment, index) => {
+        return this.props.comments.map((comment, index) => {
 
             return (
                 <Comment
@@ -153,10 +93,10 @@ class App extends Component {
 
 
     render() {
+        console.log(this.props)
         return (
             <div className={classes.App}>
                 <h1>Виджет Комментариев</h1>
-
                 <CommentsList>
                     {
                         this.renderMessage()
@@ -165,30 +105,46 @@ class App extends Component {
 
                 <TextArea
                     placeholder='Введите текст сообщения'
-                    value={this.state.comment.commentText}
-                    onChangeTextArea={this.onTextAreaChangeHandler}
-                    valid={this.state.textArea.valid}
-                    touched={this.state.textArea.touched}
-                    errorMessage={this.state.textArea.errorMessage}
+                    value={this.props.comment.commentText}
+                    onChangeTextArea={this.props.onTextAreaChangeHandler}
+                    valid={this.validateControl(this.props.textArea.valid)}
+                    touched={this.props.textArea.touched}
+                    errorMessage={this.props.textArea.errorMessage}
                 />
                 <Input
                     placeholder='Введите ваше имя'
                     type="text"
-                    value={this.state.comment.name}
-                    onInputChange={this.onInputChangeHandler}
-                    valid={this.state.input.valid}
-                    touched={this.state.input.touched}
-                    errorMessage={this.state.input.errorMessage}
+                    value={this.props.comment.name}
+                    onInputChange={this.props.onInputChangeHandler}
+                    valid={this.props.input.valid}
+                    touched={this.props.input.touched}
+                    errorMessage={this.props.input.errorMessage}
                 />
                 <Button
                     onClickBtn={this.onClickBtnHandler}
-                    disabled={!this.state.fieldsValid}
+                    disabled={!this.props.fieldsValid}
                 />
-
             </div>
 
         )
     }
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        fieldsValid: state.fieldsValid,
+        comments: state.comments,
+        comment: state.comment,
+        textArea: state.textArea,
+        input: state.input
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onTextAreaChangeHandler: (e) => dispatch(onTextAreaChangeHandler(e)),
+        onInputChangeHandler: (e) => dispatch(onInputChangeHandler(e))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
